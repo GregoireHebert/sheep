3518ede34e23c440d2ab7e74db0361e2b555ec3e
Grégoire Hébert <gregoire@les-tilleuls.coop>
2018-03-13 23:06:28 +0100

    Add WebSocket and dynamic hud

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
 import Sheep from './Sheep';
 import Hud from './Hud';
+import $ from 'jquery';
 
 export default class Game extends Component {
   state = {
-    animation: null
+    animation: null,
+    health: null,
+    hunger: null,
+    playful: null,
+    sleepiness: null,
+    weight: null,
+    wealth: null,
   };
 
-  componentWillMount () {
-    // Fixtures
-    const anims = ['run', 'eat', 'walk', 'die', 'idle'];
-    this.setState(() =>  ({ animation: anims[Math.floor(Math.random()*anims.length)] }));
+  componentDidMount() {
+      const that = this;
+      const websocket = WS.connect('ws://127.0.0.1:1337/');
 
-    setInterval(() => {
-      this.setState(() =>  ({ animation: anims[Math.floor(Math.random()*anims.length)] }));
-    }, 20000);
+      websocket.on("socket/connect", function(session){
+          console.log("connection OK");
+
+          session.subscribe("output/application", function(uri, payload){
+              console.log("Received message", payload);
+              console.log(payload);
+
+              that.setState(() => ({
+                  animation: payload.slice(1,-1)
+              }));
+
+              $.get("http://127.0.0.1:8000/tamagotchi", function( tamagotchi ) {
+                  console.table(tamagotchi);
+
+                  this.setState(() => ({
+                      health: tamagotchi.health,
+                      hunger: tamagotchi.hunger,
+                      playful: tamagotchi.playful,
+                      sleepiness: tamagotchi.sleepiness,
+                      weight: tamagotchi.weight,
+                      wealth: tamagotchi.wealth,
+                      animation: payload
+                  }));
+              });
+          });
+      });
+
+      websocket.on("socket/disconnect", function(error){
+          //error provides us with some insight into the disconnection: error.reason and error.code
+          console.log("Disconnected for " + error.reason + " with code " + error.code);
+      });
   }
 
   render () {
@@ -22,7 +56,14 @@ export default class Game extends Component {
       <div id="world">
         <div id="sky">&nbsp;</div>
         <div id="grass_front">&nbsp;</div>
-        <Hud />
+        <Hud
+            health={this.state.health}
+            hunger={this.state.hunger}
+            playful={this.state.playful}
+            sleepiness={this.state.sleepiness}
+            weight={this.state.weight}
+            wealth={this.state.wealth}
+        />
         <Sheep animation={this.state.animation} />
 
         <div id="grass_back">&nbsp;</div>
--- src/components/Hud.js
+++ src/components/Hud.js
@@ -1,42 +1,66 @@
 import React, { Component } from 'react';
 
 export default class Hud extends Component {
-  state = {
-    hearts: null
-  };
+    state = {
+        health: null,
+        hunger: null,
+        playful: null,
+        sleepiness: null,
+        weight: null,
+        wealth: null,
+    };
+
+    updateHud = (props) => {
+        this.setState(() => ({
+            health: props.health,
+            hunger: props.hunger,
+            playful: props.playful,
+            sleepiness: props.sleepiness,
+            weight: props.weight,
+            wealth: props.wealth
+        }));
+    };
+
+    componentDidMount() {
+        this.updateHud(this.props);
+    }
+
+    componentWillReceiveProps(nextProps)  {
+        this.updateHud(nextProps);
+    };
 
   render () {
     return (
       <div id="hud">
-        <div className="heart">&nbsp;</div>
-        <div className="heart">&nbsp;</div>
-        <div className="heart">&nbsp;</div>
-        <div className="heart inactive">&nbsp;</div>
-        <div className="heart inactive">&nbsp;</div>
+        <div className={`heart ${this.state.health <= 0 && 'inactive'}`}>&nbsp;</div>
+        <div className={`heart ${this.state.health <= 2 && 'inactive'}`}>&nbsp;</div>
+        <div className={`heart ${this.state.health <= 4 && 'inactive'}`}>&nbsp;</div>
+        <div className={`heart ${this.state.health <= 6 && 'inactive'}`}>&nbsp;</div>
+        <div className={`heart ${this.state.health <= 8 && 'inactive'}`}>&nbsp;</div>
 
         <div className="spacing">&nbsp;</div>
 
-        <div className="hungry">&nbsp;</div>
-        <div className="hungry">&nbsp;</div>
-        <div className="hungry">&nbsp;</div>
-        <div className="hungry inactive">&nbsp;</div>
-        <div className="hungry inactive">&nbsp;</div>
+        <div className={`hungry ${this.state.hunger <= 0 && 'inactive'}`}>&nbsp;</div>
+        <div className={`hungry ${this.state.hunger <= 2 && 'inactive'}`}>&nbsp;</div>
+        <div className={`hungry ${this.state.hunger <= 4 && 'inactive'}`}>&nbsp;</div>
+        <div className={`hungry ${this.state.hunger <= 6 && 'inactive'}`}>&nbsp;</div>
+        <div className={`hungry ${this.state.hunger <= 8 && 'inactive'}`}>&nbsp;</div>
 
         <div className="spacing">&nbsp;</div>
 
-        <div className="playful">&nbsp;</div>
-        <div className="playful">&nbsp;</div>
-        <div className="playful inactive">&nbsp;</div>
-        <div className="playful inactive">&nbsp;</div>
-        <div className="playful inactive">&nbsp;</div>
+        <div className={`playful ${this.state.playful <= 0 && 'inactive'}`}>&nbsp;</div>
+        <div className={`playful ${this.state.playful <= 2 && 'inactive'}`}>&nbsp;</div>
+        <div className={`playful ${this.state.playful <= 4 && 'inactive'}`}>&nbsp;</div>
+        <div className={`playful ${this.state.playful <= 6 && 'inactive'}`}>&nbsp;</div>
+        <div className={`playful ${this.state.playful <= 8 && 'inactive'}`}>&nbsp;</div>
 
         <div className="spacing">&nbsp;</div>
 
-        <div className="sleep">&nbsp;</div>
-        <div className="sleep">&nbsp;</div>
-        <div className="sleep inactive">&nbsp;</div>
-        <div className="sleep inactive">&nbsp;</div>
-        <div className="sleep inactive">&nbsp;</div>
+        <div className={`sleep ${this.state.sleepiness <= 0 && 'inactive'}`}>&nbsp;</div>
+        <div className={`sleep ${this.state.sleepiness <= 2 && 'inactive'}`}>&nbsp;</div>
+        <div className={`sleep ${this.state.sleepiness <= 4 && 'inactive'}`}>&nbsp;</div>
+        <div className={`sleep ${this.state.sleepiness <= 6 && 'inactive'}`}>&nbsp;</div>
+        <div className={`sleep ${this.state.sleepiness <= 8 && 'inactive'}`}>&nbsp;</div>
       </div>
     );
   }
